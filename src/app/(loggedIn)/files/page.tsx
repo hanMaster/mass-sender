c4dd4793d54a207ce0e43eda1638c662014b98b3
hanmaster <w54661c@gmail.com>
2025-10-09 08:55:36 +1000

    fill template implemented

--- src/app/(loggedIn)/files/page.tsx
+++ src/app/(loggedIn)/files/page.tsx
@@ -1,4 +1,6 @@
 import {SiteHeader} from "@/components/site-header";
+import {Button} from "@/components/ui/button";
+import {prepareFile} from "@/lib/fillTemplate";
 
 export default async function FilesPage() {
     return (
@@ -8,6 +10,7 @@ export default async function FilesPage() {
                 <div className="@container/main flex flex-1 flex-col gap-2">
                     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                         <h1>Файлы уведомлений</h1>
+                        <Button className='bg-primary' onClick={prepareFile}>Создать файл уведомления</Button>
                     </div>
                 </div>
             </div>
---
+++ src/lib/fillTemplate.ts
@@ -0,0 +1,41 @@
+'use server'
+
+// Load our library that generates the document
+import Docxtemplater from 'docxtemplater'
+// Load PizZip library to load the docx/pptx/xlsx file in memory
+import PizZip from "pizzip";
+import fs from 'fs';
+import path from 'path';
+
+export async function prepareFile() {
+
+    // Load the docx file as binary content
+    const templatePath = path.join(process.cwd(), 'templates', 'tpl_1759905729022.docx');
+    console.log('filePath', templatePath);
+    const content = fs.readFileSync(templatePath, "binary");
+
+    // Unzip the content of the file
+    const zip = new PizZip(content);
+
+    /*
+     * Parse the template.
+     * This function throws an error if the template is invalid,
+     * for example, if the template is "Hello {user" (missing closing tag)
+     */
+    const doc = new Docxtemplater(zip, {paragraphLoop: true, linebreaks: true,});
+
+    /*
+     * Render the document : Replaces :
+     * - {first_name} with John
+     * - {last_name} with Doe,
+     * ...
+     */
+    doc.render({
+        date: new Date().toLocaleDateString(),
+    });
+
+    const buf = doc.toBuffer();
+    const filePath = path.join(process.cwd(), 'output', 'output.docx');
+    // Write the Buffer to a file
+    fs.writeFileSync(filePath, buf);
+}
